4e857157cbe3a537be619bf6131a0c60e5812d1a
Chadi Jamal <cjamal@e2r10p12.1337.ma>
2019-04-30 17:49:07 +0100

    egeg

--- verifchadi.c
+++ verifchadi.c
@@ -6,7 +6,7 @@
 /*   By: iel-ferk <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/28 15:21:44 by iel-ferk          #+#    #+#             */
-/*   Updated: 2019/04/28 18:05:11 by iel-ferk         ###   ########.fr       */
+/*   Updated: 2019/04/30 13:23:03 by cjamal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,67 +28,47 @@ int		ft_char_validity(char *str)
 
 int main(int  ac, char **av)
 {
-	int fd1, fd2;
-	int r1 = 1, r2;
+	int fd1;
+	int r1 = 1;
 	int i = 0;
 	int flag = 1;
-	char *line;
+	char tmp[2]= "\n";
 	char buff1[TETRO_SIZE + 1];
-	char buff2[6];
 	char **split;
 
-//	if (ac == 2)
-//	{
-		fd1 = open(av[1],O_RDONLY);
-		//fd2 = open(av[2],O_RDONLY);
-		/*
-		while (get_next_line(fd, &line))
-		{
-			printf("%s\n",line );
-			//free(line);
-		}
-		//free(line);		*/
+	fd1 = open(av[1],O_RDONLY);
 
-		// comparaison between input testfile 
-		//	and a valid tetro
-		//
-		/*
-		while ((r1 = read(fd1, buff1, 4)) && (r2 = read(fd2, buff2, 4)) && i < 5)
+	// function that verifies the structure of the FILE
+	// 20 char/tetro + \n between two tetros 
+	// 
+	while ((tmp[0] == '\n') && (r1 = read(fd1, buff1, TETRO_SIZE)) > 0)
+	{
+		buff1[r1] = '\0';
+		split = ft_strsplit(buff1, '\n');
+		i = -1;
+		//while (split[++i])
+		//	if (!ft_char_validity(split[i]))
+		//		break;
+		
+		while((split[++i]) && (ft_strlen(split[i]) == 4))
+			;
+		//printf("%d\n", i);
+		if (i != 4)
 		{
-			buff1[r1] = '\0';
-			buff2[r2] = '\0';
-			
-			if(ft_strcmp(buff1, buff2) != 0)
-				flag  = 0;
-			i++;
+			flag = 3;
+			break;
 		}
-		*/
-		// verification of '#' and '.'
-		//
-		while ((r1 = read(fd1, buff1,TETRO_SIZE)) > 0)
-		{
-			buff1[r1] = '\0';
-			split = ft_strsplit(buff1, '\n');
-			i = -1;
-			while (split[++i])
-				if (!ft_char_validity(split[i]))
-					break;
-			if (i != 4)
-			{
-				flag = 3;
-				break;
-			}
-			if (!(r1 = read(fd1, buff1, 1)))
-				break;
-			buff1[r1] = '\0';
-		}
-		if (flag == 1)
-			printf("Is a valid File\n");
-		else if (flag == 3)
-			printf("Not a valid file\n");
-		else
-			printf("Is not a Titro\n");
-		if (close(fd1) == -1)
-			printf("close error");
+		if (!(r1 = read(fd1, tmp, 1)))
+			break;
+		tmp[r1] = '\0';
+	}
+	if (flag == 1)
+		printf("Is a valid File\n");
+	else if (flag == 3 || (tmp[0] == '\n'))
+		printf("Not a valid file\n");
+	else
+		printf("Is not a Titro\n");
+	if (close(fd1) == -1)
+		printf("close error");
 	return (0);
 }
