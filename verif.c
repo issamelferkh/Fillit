b033a11ce16decfd0ca9f64e2d289bbc7ed4068f
Issam El ferkh <iel-ferk@e2r10p13.1337.ma>
2019-05-08 01:26:51 +0000

    verif titro -prob des blocks-

--- verif.c
+++ verif.c
@@ -16,16 +16,14 @@ int main(int  ac, char **av)
 {
 	int fd1, fd2;
 	int r1, r2;
-	int i = 0;
-	int flag = 0;
+	int i, j = 1, cpt = 0;
+	int flag;
 	char *line;
-	char buff1[5];
-	char buff2[5];
+	char *titro;
 
 //	if (ac == 2)
 //	{
-		fd1 = open(av[1],O_RDONLY);
-		fd2 = open(av[2],O_RDONLY);
+		
 		/*
 		while (get_next_line(fd, &line))
 		{
@@ -33,19 +31,39 @@ int main(int  ac, char **av)
 			//free(line);
 		}
 		//free(line);		*/
-
-		while ((r1 = read(fd1, buff1, 4)) && (r2 = read(fd2, buff2, 4)) && i < 5)
+		fd1 = open(av[1],O_RDONLY);
+		printf("* Input file name: %s\n",av[1]);
+		flag = 0;
+		while (++cpt <= 19)
 		{
-			buff1[r1] = '\0';
-			buff2[r2] = '\0';
-			if(ft_strcmp(buff1, buff2) != 0)
-				flag  = 1;
-			i++;
+			
+			printf("* Titros file name: %s\n",ft_itoa(cpt));
+			fd2 = open(ft_itoa(cpt),O_RDONLY);
+			i = 0;
+			printf("* i : %d\n",i);
+			while (get_next_line(fd1, &line) && get_next_line(fd2, &titro) && i < 4)
+			{
+				if(ft_strcmp(line, titro) != 0)
+					flag  = 1;
+				i++;
+				printf("### flag = %d\n",flag);
+			}
+			// i = 0;
+			// //buff1 = {0,0,0,0,0};
+			// r2 = 0;
+			if (flag  == 0)
+			{
+				printf("+++ The Block %d is found in Titro %d +++\n", j,cpt);
+				//break;
+			}
+			else
+			{
+				printf("!!! The Block %d is not found in Titro %d !!!\n", j,cpt);
+			}
+			//j = j + 5;
 		}
-		if (flag  == 0)
-			printf("Is a Titro\n");
-		else
-			printf("Is not a Titro\n");
+		// if (flag != 0)
+		// 	printf("Is not a Titro\n");
 
 //	}
 //	else
